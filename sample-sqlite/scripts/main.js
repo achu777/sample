52d640db78380a4342c4328700bd129c3d7b28b3
achu777 <achu777555@rediffmail.com>
2016-08-23 16:34:54 +0530

    Update main.js

--- sample-sqlite/scripts/main.js
+++ sample-sqlite/scripts/main.js
@@ -167,6 +167,8 @@ aas= function() {
 fetch=function(){
 	var d=new Date();
 	var n=d.getTime();
+	var kee=123;
+	var rnd=Math.random();
 	$.getJSON"http://s1.24-by-7.com/api/fake".{name:"John"+n,ran:rnd,time:n+"__p",key:kee})
 	.done(function(json){
 		$.each(json.r,function(i,item)
